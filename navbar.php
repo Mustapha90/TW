1b565320500b920a2cc689e2ff47b34fe717bb8a
Bilal Yesfi Daimoussi <bilalgr@correo.ugr.es>
2017-06-13 14:43:39 +0200

    cambio de menu emergente y de formulario

--- navbar.php
+++ navbar.php
@@ -11,9 +11,21 @@
 	<div id="espacio"></div>
 	
 	<div id="nav2">
-		<ul>
-			<li><a href="index.php?sec=addpub" >Añadir pub</a></li>
-			<li><a href="index.php?sec=addproyecto">Añadir proyecto</a></li>
+		<ul class="mainmenu">
+			<li><a href="index.php?sec=addpub" >Añadir pub</a>
+				<ul class="submenu">
+					<li><a href="index.php?sec=adduser">Añadir pub</a></li>
+					<li><a href="">Editar pub</a></li>
+					<li><a href="">Borrar pub</a></li>
+				</ul>		
+			</li>
+			<li><a href="index.php?sec=addproyecto">Añadir proyecto</a>
+				<ul class="submenu">
+					<li><a href="index.php?sec=adduser">Añadir proyecto</a></li>
+					<li><a href="">Editar proyecto</a></li>
+					<li><a href="">Borrar proyecto</a></li>
+				</ul>					
+			</li>
 		</ul>						
 	</div>
 
@@ -23,19 +35,21 @@
 
 	<?php if(isset($_SESSION['loggedin']) and $_SESSION['admin']==1) : ?>
 		<div id="nav1">
-			<ul>
-				<li><a href="index.php?sec=editarusuarios">Editar usuarios</a></li>
+			<ul class="mainmenu">
+				<li><a>Editar usuarios</a>
+					<ul class="submenu">
+						<li><a href="index.php?sec=adduser">Añadir miembro</a></li>
+						<li><a href="">Editar usuario</a></li>
+						<li><a href="">Borrar usuario</a></li>
+			  		</ul>
+				</li>
 				<li><a href="index.php?sec=verlog">Ver log</a></li>
 				<li><a href="index.php?sec=backup">Realizar un backup</a></li>
 				<li><a href="index.php?sec=restore">Restaurar el backup</a></li>
 			</ul>					
 		</div>
 	<?php endif; ?>
-
-
-
-
-
+	
 
 </nav>
 
--- src/adduser.php
+++ src/adduser.php
@@ -1,142 +1,151 @@
-<?php
+
+<h3>Añadir miembro</h3>
+</div>
 
 
-require 'src/dbConnect.php';
-
-set_time_limit(0);
-ini_set('upload_max_filesize', '500M');
-ini_set('post_max_size', '500M');
-ini_set('max_input_time', 4000);
-ini_set('max_execution_time', 4000);
+<div id="centro_content">
+	<?php
 
 
-$target_dir = "images/";
-$imageFileType = '';
-$target_file='';
+	require 'src/dbConnect.php';
 
-$emailError='';
-$passwordError='';
-$passwordConfError='';
-$email='';
-$nombre='';
-$password='';
-$imageError='';
+	set_time_limit(0);
+	ini_set('upload_max_filesize', '500M');
+	ini_set('post_max_size', '500M');
+	ini_set('max_input_time', 4000);
+	ini_set('max_execution_time', 4000);
 
 
-$apellidos='';
-$categoria='';
-$telefono=''; 
-$url='';
-$departamento=''; 
-$centro='';
-$instituto=''; 
-$direccion='';
-$imagen='';
+	$target_dir = "images/";
+	$imageFileType = '';
+	$target_file='';
+
+	$emailError='';
+	$passwordError='';
+	$passwordConfError='';
+	$email='';
+	$nombre='';
+	$password='';
+	$imageError='';
 
 
-function validateForm(){
-	$errores=0;
-	global $emailError, $passwordError,$passwordConfError, $email, $password;
-	global $nombre, $email, $password, $nombre, $apellidos, $categoria, $telefono, $url, $departamento, $centro, $instituto, $direccion, $target_dir, $target_file, $imageFileType, $imagen, $imageError;
+	$apellidos='';
+	$categoria='';
+	$telefono=''; 
+	$url='';
+	$departamento=''; 
+	$centro='';
+	$instituto=''; 
+	$direccion='';
+	$imagen='';
 
 
-	if (isset($_POST['email'])) {
-        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailError =  "Introduzca un email válido!"; 
-						$errores=1;
-        }
-				else{
-						$existeEmail=dbConnect("SELECT * FROM usuarios WHERE email='$email'");
-						if(mysqli_num_rows($existeEmail) == 1){ 
-							$emailError =  "Este email ya existe!"; 
+	function validateForm(){
+		$errores=0;
+		global $emailError, $passwordError,$passwordConfError, $email, $password;
+		global $nombre, $email, $password, $nombre, $apellidos, $categoria, $telefono, $url, $departamento, $centro, $instituto, $direccion, $target_dir, $target_file, $imageFileType, $imagen, $imageError;
+
+
+		if (isset($_POST['email'])) {
+			$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				$emailError =  "Introduzca un email válido!"; 
 							$errores=1;
-						}
-				}
-   }
-
-	if (isset($_POST['password'])){
-		$password=$_POST['password'];
-		if(strlen($password)<6) { 
-			$passwordError="La contraseña debe contener al menos 6 caracteres";
-			$errores=1;
-		}
-		else{
-			if (isset($_POST['passwordConf'])){
-				$passwordConf=$_POST['passwordConf'];
-				if($password != $passwordConf) { 
-					$passwordConfError="Las contraseñas no coinciden";
-					$errores=1;
 			}
+					else{
+							$existeEmail=dbConnect("SELECT * FROM usuarios WHERE email='$email'");
+							if(mysqli_num_rows($existeEmail) == 1){ 
+								$emailError =  "Este email ya existe!"; 
+								$errores=1;
+							}
+					}
+	   }
+
+		if (isset($_POST['password'])){
+			$password=$_POST['password'];
+			if(strlen($password)<6) { 
+				$passwordError="La contraseña debe contener al menos 6 caracteres";
+				$errores=1;
+			}
+			else{
+				if (isset($_POST['passwordConf'])){
+					$passwordConf=$_POST['passwordConf'];
+					if($password != $passwordConf) { 
+						$passwordConfError="Las contraseñas no coinciden";
+						$errores=1;
+				}
+			}
+			}
+
 		}
-		}
-		
+
+	$nombre=$_POST['nombre'];
+	$apellidos=$_POST['apellidos'];
+	$categoria=$_POST['categoria'];
+	$telefono=$_POST['telefono']; 
+	$url=$_POST['url'];
+	$departamento=$_POST['departamento']; 
+	$centro=$_POST['centromus'];
+	$instituto=$_POST['instituto']; 
+	$direccion=$_POST['direccion'];
+
+
+
+	$imagen = $email . "_" . basename($_FILES["fileToUpload"]["name"]);
+	$target_file = $target_dir . $imagen;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+
+	if ($_FILES["fileToUpload"]["size"] > 500000) {
+		$imageError=  "El tamaño del fichero supera el tamaño máximo.";
+		$errores = 1;
 	}
 
-$nombre=$_POST['nombre'];
-$apellidos=$_POST['apellidos'];
-$categoria=$_POST['categoria'];
-$telefono=$_POST['telefono']; 
-$url=$_POST['url'];
-$departamento=$_POST['departamento']; 
-$centro=$_POST['centromus'];
-$instituto=$_POST['instituto']; 
-$direccion=$_POST['direccion'];
-	
 
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	&& $imageFileType != "gif" ) {
+		$imageError =  "Solo se permiten los formatos, JPG, JPEG, PNG y GIF.";
+		$errores = 1;
+	}
+		return $errores;
 
-$imagen = $email . "_" . basename($_FILES["fileToUpload"]["name"]);
-$target_file = $target_dir . $imagen;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
-
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    $imageError=  "El tamaño del fichero supera el tamaño máximo.";
-    $errores = 1;
-}
-
-
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    $imageError =  "Solo se permiten los formatos, JPG, JPEG, PNG y GIF.";
-    $errores = 1;
-}
-	return $errores;
-
-}
-
-
-function register(){
-	global $nombre, $email, $password, $nombre, $apellidos, $categoria, $telefono, $url, $departamento, $centro, $instituto, $direccion, $target_dir, $target_file, $imageFileType, $imagen;
-	$hash = password_hash($password, \PASSWORD_DEFAULT);
-
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-
-	$insert = dbConnect("INSERT INTO usuarios (nombre, email, password, apellidos, categoria, telefono, url, departamento, centro, instituto, direccion, imagen) VALUES ('$nombre','$email','$hash', '$apellidos', '$categoria', '$telefono', '$url', '$departamento', '$centro', '$instituto', '$direccion', '$imagen')");
-
-	if(!$insert){
-		die("No se ha podido registrar el usuario: ");
 	}
 
-	echo $nombre.", you are now registered. Thank you!<br><a href=login.php>Login</a> | <a href=index.php>Index</a>";
-}
+
+	function register(){
+		global $nombre, $email, $password, $nombre, $apellidos, $categoria, $telefono, $url, $departamento, $centro, $instituto, $direccion, $target_dir, $target_file, $imageFileType, $imagen;
+		$hash = password_hash($password, \PASSWORD_DEFAULT);
+
+		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		} else {
+			echo "Sorry, there was an error uploading your file.";
+		}
+
+		$insert = dbConnect("INSERT INTO usuarios (nombre, email, password, apellidos, categoria, telefono, url, departamento, centro, instituto, direccion, imagen) VALUES ('$nombre','$email','$hash', '$apellidos', '$categoria', '$telefono', '$url', '$departamento', '$centro', '$instituto', '$direccion', '$imagen')");
+
+		if(!$insert){
+			die("No se ha podido registrar el usuario: ");
+		}
+			echo $nombre.", you are now registered. Thank you!<br><a href=login.php>Login</a> | <a href=index.php>Index</a>";		
+
+	}
 
 
-if(isset($_GET['act'])=='register') {
-		$errores = validateForm();
-		if($errores!=0){
+	if(isset($_GET['act'])=='register') {
+			$errores = validateForm();
+			if($errores!=0){
+				include("html/adduser.html");
+			}
+			else{
+				register();
+			}
+	}else{
 			include("html/adduser.html");
-		}
-		else{
-			register();
-		}
-}else{
-		include("html/adduser.html");
-}
+	}
 
 
-?>
+	?>
+	
+	
+</div>
