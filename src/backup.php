3439e6385563cf5c078cd02d7fdceba823f83ad3
Mustapha <mj4ever001@gmail.com>
2017-06-14 18:56:12 +0200

    Aniadir cambios

--- src/backup.php
+++ src/backup.php
@@ -11,17 +11,16 @@ $password      = DB_PASSWORD;
 $database_name = DB_NAME;
 
 
-
-$date_string   = date("dmYhis");
-$filename = BACKUP_PATH . $date_string . "_" . $database_name . ".sql";
-
-
-$cmd = "mysqldump --routines -h {$server_name} -u {$username} -p{$password} {$database_name} >  {$filename}";
-
-
-exec($cmd);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$date_string   = date("dmYhis");
+	$file = $date_string . "_" . $database_name . ".sql";
+	$filename = BACKUP_PATH . $file;
+	$cmd = "mysqldump --routines -h {$server_name} -u {$username} -p{$password} {$database_name} >  {$filename}";
+	exec($cmd);
+}
 
 ?>
+
 
 
 <h3>Realizar Backup</h3>
@@ -29,8 +28,23 @@ exec($cmd);
 
 <div id="centro_content">
 
-	<div id="backup_content">
-		 <h4 id="backup_h4"> Haz click para descargar el backup : <a href="<?php echo $filename?>"> <button id="btnbackup">Descargar backup </button> </a> </h4>
-	</div>
+	<?php if($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+		<div id="backup_content">
+			 <h4 id="backup_h4">Backup realizado correctamente : <?php echo $file?> <a href="<?php echo $filename?>"> <button id="btnbackup">Descargar backup</button> </a> </h4>
+		</div>
+	<?php else: ?>
+		<form action="" method="post">
+  			<input type="submit" value="Realizar Backup">
+		</form>
+	<?php endif; ?>
+
+
+
+
+
+
  	
 </div>
+
+
+
---
+++ src/restore.php
@@ -0,0 +1,67 @@
+<?php
+
+include 'src/dbconfig.php';
+
+$target_dir    = "backup/";
+$server_name   = DB_HOST;
+$username      = DB_USER;
+$password      = DB_PASSWORD;
+$database_name = DB_NAME;
+
+$errorUpload='';
+$success=false;
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$date_string   = date("dmYhis");
+    $file = $date_string . "_" . basename($_FILES["fileToUpload"]["name"]);
+	$target_file = $target_dir . $file;
+
+	if(validarFichero($target_file)==0){
+		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			$cmd = "mysql -h {$server_name} -u {$username} -p{$password} {$database_name} < $target_file";
+			exec($cmd);
+			$success = true;
+		} else {
+			echo "Error: No se ha podido subir el archivo al servidor en estos momentos!";
+		}
+	}
+}
+
+function validarFichero($target_file){
+	global $errorUpload;
+
+	$errores = 0;
+	$fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+
+	if ($_FILES["fileToUpload"]["size"] > 500000) {
+		$errorUpload = "El tamaño del fichero supera el tamaño máximo.";
+		$errores = 1;
+	}
+
+
+	if($fileType != "sql") {
+		$errorUpload = "Error: No es un fichero sql!";
+		$errores = 1;
+	}
+
+	return $errores;
+}
+?>
+
+<h3>Restaurar Backup</h3>
+</div>
+<div id="centro_content">
+	<?php if($success): ?>
+		<h2>La copia de seguridad se ha realizado correctamente!</h2>
+	<?php else: ?>
+			<form action="" method="post" enctype="multipart/form-data">
+				<label for="fileToUpload">Restaurar copia de seguridad:</label>
+				<input type="file" name="fileToUpload" id="fileToUpload">
+				<div class="errorlabel"<label for="passError"><?php echo $errorUpload;?></label></div>
+  				<input type="submit" value="Restaurar">
+		    </form>
+	<?php endif; ?>
+</div>
+
